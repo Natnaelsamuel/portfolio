bd6a9d5243e746aa4fff2244d62bff7dd4ee50dd
Natnaelsamuel <natisami907@gmail.com>
2025-07-17 15:43:56 +0300

    made the header text a fuzzytext

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,28 +1,56 @@
 "use client";
 import DotGrid from "@/components/backgrounds/DotGrid/DotGrid";
+import Particles from "@/components/backgrounds/Particles/Particles";
 import RippleGrid from "@/components/backgrounds/RippleGrid/RippleGrid";
 import Squares from "@/components/backgrounds/Squares/Squares";
+import FuzzyText from "@/components/TextAnimations/FuzzyText/FuzzyText";
+import { Button } from "@/components/ui/button";
+import { useTheme } from "next-themes";
 
 const Hero = () => {
+  const { theme } = useTheme();
   return (
     <div className="relative w-screen h-screen overflow-hidden">
       <div className="absolute inset-0 z-0">
-        <DotGrid
-            dotSize={2}
-            gap={24}
-            baseColor="#aaa"
-            activeColor="#5227FF"
-            proximity={120}
-            shockRadius={250}
-            shockStrength={5}
-            resistance={750}
-            returnDuration={1.5}
-          />
+        {/* <DotGrid
+          dotSize={2}
+          gap={24}
+          baseColor="#aaa"
+          activeColor="#5227FF"
+          proximity={120}
+          shockRadius={250}
+          shockStrength={5}
+          resistance={750}
+          returnDuration={1.5}
+        /> */}
+        <Particles
+          particleColors={["#ffffff", "#ffffff"]}
+          particleCount={500}
+          particleSpread={10}
+          speed={0.5}
+          particleBaseSize={100}
+          moveParticlesOnHover={true}
+          alphaParticles={false}
+          disableRotation={false}
+        />
       </div>
 
-      <div className="relative z-50 flex flex-col items-center justify-center h-full text-black dark:text-white text-center">
-        <h1 className="text-5xl font-bold mb-4">Hi, I'm Natnael Samuel</h1>
-        <p className="text-xl">Full-Stack Developer | UI Enthusiast</p>
+      <div className="relative z-50 flex flex-col gap-1 items-center justify-center h-full text-black dark:text-white text-center">
+        <h1 className="text-5xl font-bold mb-4 dark:text-white">
+          Hi,
+          <FuzzyText
+            baseIntensity={0.4}
+            hoverIntensity={0.1}
+            enableHover={true}
+            fontSize={"3.7rem"}
+            color={theme === "dark" ? "#fff" : "#000"}
+            fontWeight={700}
+          >
+            I'm Natnael Samuel
+          </FuzzyText>
+        </h1>
+        <p className="text-xl mb-3">Full-Stack Developer | UI Enthusiast</p>
+        <Button variant="default">About me</Button>
       </div>
     </div>
   );
--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,11 +1,20 @@
-'use client';
+"use client";
+import GooeyNav from "@/components/Components/GooeyNav/GooeyNav";
 import { ModeToggle } from "./ModeToggle";
+import { useTheme } from "next-themes";
+
+const items = [
+  { label: "Home", href: "#" },
+  { label: "About", href: "#" },
+  { label: "Contact", href: "#" },
+];
 
 const NavBar = () => {
+  const { theme } = useTheme();
   return (
-    <nav className="flex justify-between items-center px-8 py-4 bg-white text-black border border-gray-500 rounded-full shadow-lg w-3xl fixed z-[51] top-8 mx-auto left-0 right-0 dark:bg-black dark:text-white dark:border-white">
+    <nav className="flex justify-between items-center px-8 py-4 bg-white text-black rounded-full shadow-black shadow-xl w-3xl sm:w-4/6 fixed z-[51] top-8 mx-auto left-0 right-0 dark:bg-black dark:shadow-white dark:shadow-md dark:text-white">
       <div>Logo</div>
-      <div>Links</div>
+      <div>Home</div>
       <ModeToggle />
     </nav>
   );
